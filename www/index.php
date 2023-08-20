d461febcfb53cab263dfd1b8b75b29f8825a055f
Eric Neves <ericnevesrodrigues@gmail.com>
2023-08-20 18:10:37 -0400

    Feat: add cors

--- www/index.php
+++ www/index.php
@@ -13,6 +13,7 @@ error_reporting(0);
 
 header('Content-type: application/json');
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Domains: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 
